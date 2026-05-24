bac64bef0b505531fcc3e51df6667b4008f4be65
Rudi Antoni <rudiantoni2013@gmail.com>
2026-05-24 00:24:10 -0300

    Added postgres connection

--- api-java-bavaria-munich/src/main/java/com/myapps/bavariamunich/ApiJavaBavariaMunichApplication.java
+++ api-java-bavaria-munich/src/main/java/com/myapps/bavariamunich/ApiJavaBavariaMunichApplication.java
@@ -2,9 +2,8 @@ package com.myapps.bavariamunich;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class ApiJavaBavariaMunichApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiJavaBavariaMunichApplication.class, args);
